38c2757bdac836aa3b78f3c86b9d0d968671b4bd
Dev.JHJ <yg9316@naver.com>
2023-02-02 17:37:07 +0900

    day04 프로젝트

---
+++ day04/out.js
@@ -0,0 +1,23 @@
+function 빨래하다() {
+    console.log("1. 먼지를 털자")
+    console.log("2. 옷을 물에 집어넣는다")
+    console.log("3. 비누를 바른다")
+    console.log("4. 오물조물 비빈다")
+    console.log("5. 물에 여러번 헹군다")
+    console.log("5. 물기를 최대한 짠다")
+    console.log("5. 햇볕에 말린다")
+}
+빨래하다()  //함수를 사용, 기능을 사용하는 것.
+빨래하다()  //함수를 사용, 기능을 사용하는 것.' */}
+
+function 청소하다() {
+    console.log("1. 창문을 연다")
+    console.log("2. 물건을 치운다")
+    console.log("3. 청소기를 돌린다")
+    console.log("4. 걸레질을 한다")
+    console.log("5. 쓰레기를 버린다")
+}
+
+청소하다()
+청소하다()
+빨래하다
---
+++ day04/out2.js
@@ -0,0 +1,15 @@
+function 여행하다() {
+    console.log("1. 여행지로 출발한다")
+    console.log("2. 맛집을 들린다")
+    console.log("3. 관광명소를 돌아다닌다")
+    console.log("4. 여행기념 선물을 산다")
+}
+function 쉬다() {
+    console.log("1. 늦잠을 잔다")
+    console.log("2. 좋아하는 음식을 먹는다")
+    console.log("3. 예능영상을 본다")
+    console.log("4. 소파에 누워있는다")
+}
+
+여행하다()
+쉬다()
